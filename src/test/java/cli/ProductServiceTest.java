2b0adba9c54a9b21547ca9fae34541142d7b5c3e
Alican <alican.oymak@unifr.ch>
2026-04-29 17:17:21 +0200

    Add additional unit tests for products

--- src/test/java/cli/ProductServiceTest.java
+++ src/test/java/cli/ProductServiceTest.java
@@ -1,17 +1,37 @@
 package cli;
 
 import org.junit.jupiter.api.Test;
+
 import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ProductServiceTest {
 
     @Test
-    void shouldReturnProducts() {
+    void shouldReturnTwoProducts() {
         ProductService service = new ProductService();
 
         List<Product> products = service.getProducts();
 
         assertEquals(2, products.size());
+    }
+
+    @Test
+    void shouldReturnFirstProductWithName() {
+        ProductService service = new ProductService();
+
+        List<Product> products = service.getProducts();
+
+        assertEquals("Tablet", products.get(0).getName());
+    }
+
+    @Test
+    void shouldReturnFirstProductWithPrice() {
+        ProductService service = new ProductService();
+
+        List<Product> products = service.getProducts();
+
+        assertEquals(32900, products.get(0).getPrice());
     }
 }
